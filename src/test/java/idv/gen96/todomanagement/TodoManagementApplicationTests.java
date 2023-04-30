50ff3c6c81aa688b7b382370082f4dedb65dbfec
96gen <96genJQ@gmail.com>
2023-04-30 16:32:41 +0800

    feat: configure application.properties

---
+++ src/test/java/idv/gen96/todomanagement/TodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.gen96.todomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
