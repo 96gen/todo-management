16bf957115d6adbb1b69258ab035c5baf6232184
96gen <96genJQ@gmail.com>
2023-04-30 18:16:38 +0800

    feat: create Todo entity

---
+++ src/main/java/idv/gen96/todomanagement/Entity/Todo.java
@@ -0,0 +1,27 @@
+package idv.gen96.todomanagement.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+}
